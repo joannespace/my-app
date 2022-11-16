f17c967fb926ccda8fa93b0d5bc5b97cc6c61ce9
Your Name <joannersq@gmail.com>
2022-11-16 21:17:13 +0700

    update branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello world</h1>
+        <h2>Testing Update Branch</h2>
       </header>
     </div>
   );
